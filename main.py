3ebf3c8c2b1c0ef25caa04bdf69c6e729c488f51
Sergey <doktorbobikyaru@gmail.com>
2023-06-29 13:51:39 +0600

    Delete change word

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputFile
-from aiogram.types.chat import ChatType
-from aiogram.dispatcher.filters import Command, Text, ChatTypeFilter
+from aiogram.dispatcher.filters import Command, Text
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils.callback_data import CallbackData
 
@@ -118,15 +117,6 @@ async def send_voice(call: types.CallbackQuery, callback_data: dict):
 
         await call.message.delete()
         await bot.send_voice(call.message.chat.id, InputFile(path))
-
-
-@dp.message_handler(ChatTypeFilter(ChatType.GROUP))
-async def replace_word(message):
-    print('ok')
-    for word in replace:
-        if word in message.text:
-            text = message.text.replace(word, replace[word])
-            await message.edit_text(text)
 
 
 async def on_startup(_):
