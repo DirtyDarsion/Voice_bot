8e8df10f59a2d028e8eab6a043c64393b1c132fc
Sergey <doktorbobikyaru@gmail.com>
2023-06-29 15:41:30 +0600

    .

--- test.py
+++ test.py
@@ -8,11 +8,7 @@ url_video = 'https://vt.tiktok.com/ZSLSA9eRR/'
 def download_tiktok(url):
     with TikTokApi() as api:
         video = api.video(url=url)
-
-        video_data = video.bytes()
-
-        with open("out.mp4", "wb") as out_file:
-            out_file.write(video_data)
+        print(video.info())
 
 
 download_tiktok(url_video)
