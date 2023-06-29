9161f01ba54de48cddc220b542514e59e24adf4b
Sergey <doktorbobikyaru@gmail.com>
2023-06-29 17:22:42 +0600

    .

--- download_video.py
+++ download_video.py
@@ -25,7 +25,7 @@ def download_youtube_video(url):
 
 
 def download_tiktok_video(url):
-    api = TikTokApi(custom_verify_fp=TIKTOK_VERIFY, use_test_endpoints=True).generate_device_id()
+    TikTokApi.(custom_verify_fp=TIKTOK_VERIFY, use_test_endpoints=True)
 
     video = api.video(url=url)
     print(video.id)
