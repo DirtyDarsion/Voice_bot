80f6bbd4898d186fe385ad3188a36715012db970
dirtydarsion <doktorbobikyaru@gmail.com>
2024-08-16 18:31:37 +0600

    Add webhook

--- main.py
+++ main.py
@@ -1,30 +1,66 @@
 import os
 import asyncio
 
+from aiohttp import web
+
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.bot import DefaultBotProperties
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
 from config import TOKEN
 from logger import logname
 from routers import service, voice_to_text, mem, weather
 
+WEB_SERVER_HOST = "127.0.0.1"
+WEB_SERVER_PORT = 8080
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-dp.include_routers(
-    service.router,
-    voice_to_text.router,
-    mem.router,
-    weather.router,
-)
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_SECRET = "my-secret"
+BASE_WEBHOOK_URL = "https://serjo.site"
+
+
+async def on_startup(bot: Bot) -> None:
+    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}", secret_token=WEBHOOK_SECRET)
 
 
 async def main() -> None:
     print(f'Path to log: {os.getcwd()}/{logname}')
-    print('Start polling...')
 
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    dp = Dispatcher()
+    dp.include_routers(
+        service.router,
+        voice_to_text.router,
+        mem.router,
+        weather.router,
+    )
+
+    # Register startup hook to initialize webhook
+    dp.startup.register(on_startup)
+
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # Create aiohttp.web.Application instance
+    app = web.Application()
+
+    # Create an instance of request handler,
+    # aiogram has few implementations for different cases of usage
+    # In this example we use SimpleRequestHandler which is designed to handle simple cases
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=WEBHOOK_SECRET,
+    )
+    # Register webhook handler on application
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+
+    # Mount dispatcher startup and shutdown hooks to aiohttp application
+    setup_application(app, dp, bot=bot)
+
+    # And finally start webserver
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
