aaeebc1ed8d5a5151c9266c29745fd6554161094
dirtydarsion <doktorbobikyaru@gmail.com>
2024-08-20 18:42:34 +0600

    Change voice to text

--- routers/voice_to_text.py
+++ routers/voice_to_text.py
@@ -20,41 +20,30 @@ router = Router()
 
 
 def voice_to_text(path) -> str:
-    print('START')
     new_path = path[:-3] + 'wav'
 
     # Read mp3
     audio_file = AudioSegment.from_file(path)
 
     # Cut the audio track if it is longer than 90 seconds
-    count = int(audio_file.duration_seconds // 90 + (1 if audio_file.duration_seconds % 90 else 0))
+    audio_len = audio_file.duration_seconds
+    count = int(audio_len // 90 + (1 if audio_len % 90 else 0))
     output = ''
 
     # Get text in wav
     r = speech_recognition.Recognizer()
-    print('START cycle')
     for i in range(count):
-        print(f'START cycle {i}')
         temp_audio = audio_file[90000 * i:90000 * (i + 1)]
-        print(f'.')
         temp_audio.export(new_path, format="wav")
-        print(f'..')
         with speech_recognition.AudioFile(new_path) as file:
-            print(f'...')
             audio = r.record(file)
-            print(f'....')
             r.adjust_for_ambient_noise(file)
-            print(f'.....')
             test = r.recognize_google(audio, language="ru-RU")
-            print(test)
             output += test
-            print(output)
 
         os.remove(new_path)
-        print(f'END cycle {i}')
 
     os.remove(path)
-    print('END')
     return output
 
 
@@ -85,9 +74,8 @@ async def get_text_from_voice(message: Message, bot: Bot) -> None:
         temp_message = await message.reply('Ожидайте...')
         text = voice_to_text(local_path)
         print(text)
-        add_log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!__________1234qwlfkjqwsdpofjhwqurt90823u4t092erhjiowehjgwio', message, log_level=5)
+        add_log(text, message, log_level=5)
 
-        await bot.delete_message(chat_id=temp_message.chat.id, message_id=temp_message.message_id)
-        await message.reply('Готово!')
+        await temp_message.edit_text(text)
     except TypeError or AttributeError:
         await message.answer('Произошла ошибка при обработке файла.')
