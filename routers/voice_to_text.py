7133644abb31765e53344a8af59bcacc7ba2d2e7
dirtydarsion <doktorbobikyaru@gmail.com>
2024-08-27 20:25:06 +0600

    Change voice to text

--- routers/voice_to_text.py
+++ routers/voice_to_text.py
@@ -24,27 +24,40 @@ def voice_to_text(path) -> str:
 
     # Read mp3
     audio_file = AudioSegment.from_file(path)
-
-    # Cut the audio track if it is longer than 90 seconds
-    audio_len = audio_file.duration_seconds
-    count = int(audio_len // 90 + (1 if audio_len % 90 else 0))
-    output = ''
+    audio_file.export(new_path, format="wav")
 
     # Get text in wav
     r = speech_recognition.Recognizer()
-    for i in range(count):
-        temp_audio = audio_file[90000 * i:90000 * (i + 1)]
-        temp_audio.export(new_path, format="wav")
-        with speech_recognition.AudioFile(new_path) as file:
-            audio = r.record(file)
-            r.adjust_for_ambient_noise(file)
-            test = r.recognize_google(audio, language="ru-RU")
-            output += test
 
-        os.remove(new_path)
+    with speech_recognition.AudioFile(new_path) as file:
+        audio = r.record(file)
+        r.adjust_for_ambient_noise(file)
+        output = r.recognize_google(audio, language="ru-RU")
 
+    os.remove(new_path)
     os.remove(path)
     return output
+
+    # # Cut the audio track if it is longer than 90 seconds
+    # audio_len = audio_file.duration_seconds
+    # count = int(audio_len // 90 + (1 if audio_len % 90 else 0))
+    # output = ''
+    #
+    # # Get text in wav
+    # r = speech_recognition.Recognizer()
+    # for i in range(count):
+    #     temp_audio = audio_file[90000 * i:90000 * (i + 1)]
+    #     temp_audio.export(new_path, format="wav")
+    #     with speech_recognition.AudioFile(new_path) as file:
+    #         audio = r.record(file)
+    #         r.adjust_for_ambient_noise(file)
+    #         test = r.recognize_google(audio, language="ru-RU")
+    #         output += test
+    #
+    #     os.remove(new_path)
+    #
+    # os.remove(path)
+    # return output
 
 
 @router.message(F.voice)
