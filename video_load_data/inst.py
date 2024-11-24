8f9b8fbce861bfce48bf6a2d73d4890ce98bc5a1
dirtydarsion <doktorbobikyaru@gmail.com>
2024-11-24 17:53:13 +0600

    Add instagram vido download

--- video_load_data/inst.py
+++ video_load_data/inst.py
@@ -15,7 +15,10 @@ def download_iqsaved(user_download_url):
 
     url = 'https://iqsaved.com/ru/'
 
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     elem = driver.find_element(By.CLASS_NAME, 'search__input')
